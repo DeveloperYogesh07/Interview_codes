16c302e39d1d28bce964a9f8a4225f2a284c19e2
DeveloperYogesh07 <yogiupadhyay07@gmail.com>
2023-09-15 19:13:17 +0530

    adf

--- Logical/string/repeated.c
+++ Logical/string/repeated.c
@@ -1,5 +1,26 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main(){
-    int input = 20577;
+int main()
+{
+    int input = 9456123, rem;
+
+    int arr[9] = {0};
+
+    while (input > 0)
+    {
+        rem = input % 10;
+        if (arr[rem] == 1)
+        {
+            break;
+        }
+        arr[rem] = 1;
+        input = input / 10;
+    }
+
+    if(input > 0){
+        printf("yes");
+    }
+    else{
+        printf("no");
+    }
 }
